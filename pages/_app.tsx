d47d336feae3f4f1c450dfd1e891fc7e1985096c
Emily Ragan <emily.lynn.ragan@gmail.com>
2022-09-02 19:57:36 -0600

    use react-bootstrap nav bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
-import Link from 'next/link';
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 import './index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <div style={{ alignItems: 'stretch', display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
-            <div style={{ alignItems: 'center', backgroundColor: 'gray', display: 'flex', padding: 20 }}>
-                <h1 className='mr-auto'>Emily Ragan</h1>
-                <Link href='/resume'><a className='mr-auto' style={{ color: '#5a151c' }}>Resume</a></Link>
-                <Link href='/seniorproject'><a className='mr-auto' style={{ color: '#5a151c' }}>Senior Project</a></Link>
-            </div>
+            <Navbar bg='dark' variant='dark'>
+                <Navbar.Brand>Emily Ragan</Navbar.Brand>
+                <Nav className='mr-auto'>
+                    <Nav.Link href='/resume'>Resume</Nav.Link>
+                    <Nav.Link href='/seniorproject'>Senior Project</Nav.Link>
+                </Nav>
+            </Navbar>
             <Component {...pageProps} />
         </div>
     );
