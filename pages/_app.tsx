c3be9e62068f37ee640cfab7ecb8116fc5cb8fd0
Emily Ragan <emily.lynn.ragan@gmail.com>
2022-09-02 19:36:14 -0600

    make nav bar look better

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}>
+        <div style={{ alignItems: 'stretch', display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
             <div style={{ alignItems: 'center', backgroundColor: 'gray', display: 'flex', padding: 20 }}>
-                <h1>Emily Ragan</h1>
-                <Link href='/resume'><a className='mr-auto'>Resume</a></Link>
-                <Link href='/seniorproject'><a className='mr-auto'>Senior Project</a></Link>
+                <h1 className='mr-auto'>Emily Ragan</h1>
+                <Link href='/resume'><a className='mr-auto' style={{ color: '#5a151c' }}>Resume</a></Link>
+                <Link href='/seniorproject'><a className='mr-auto' style={{ color: '#5a151c' }}>Senior Project</a></Link>
             </div>
             <Component {...pageProps} />
         </div>
